10f3f310a8a315f2812fbc0b67dd06de0bdbd8c3
vismaya01 <kvvismaya98@gmail.com>
2020-12-10 21:15:43 +0530

    Modified: employee payrll class

--- js/Employee-PayRoll-Data.js
+++ js/Employee-PayRoll-Data.js
@@ -1,13 +1,11 @@
 class EmployeePayrollData {
 
-    constructor(...params) {
-        this.name = params[0];
-        this.picture = params[1];
-        this.gender = params[2];
-        this.department = params[3];
-        this.salary = params[4];
-        this.startDate = params[5];
-        this.notes = params[6];
+    //getter and setter method
+    get id() { 
+        return this._id; 
+    }
+    set id(id) { 
+        this._id = id;
     }
 
     get name() {
