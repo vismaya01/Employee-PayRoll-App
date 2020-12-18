00aea9fca0e35858237e31b6885400c4ec09dc10
vismaya01 <kvvismaya98@gmail.com>
2020-12-18 17:11:45 +0530

    Added: Reset the employee payroll from

--- js/PayRoll-form.js
+++ js/PayRoll-form.js
@@ -97,3 +97,31 @@ const getInputElementValue = (id) => {
     let value = document.getElementById(id).value;
     return value;
 }
+const resetForm = () => {
+    setValue('#name', ' ');
+    unsetSelectedValue('[name=profile]');
+    unsetSelectedValue('[name=gender]');
+    unsetSelectedValue('[name=department]');
+    setValue('#salary', '');
+    setValue('#notes', '');
+    setValue('#day', '1');
+    setValue('#month', 'January');
+    setValue('#year', '2020');
+}
+
+const unsetSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        item.checked = false;
+    });
+}
+
+const setTextValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.textContent = value;
+}
+
+const setValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.value = value;
+}
